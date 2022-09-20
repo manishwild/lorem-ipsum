e196547d4d9d4bb70e0d171bf65ea98be2d34fbb
Manish Khadgi Shahi <manishwild1000@gmail.com>
2022-09-20 09:42:50 +0200

    markdown final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import text from './data'
 
-function App() {
+const App = () => {
+  const [count, setCount] = React.useState(0)
+  const [data, setData] = React.useState([])
+
+  function submitHandler(e) {
+    e.preventDefault()
+    // count is string i m changing it to number
+  
+    let amount = parseInt(count)
+
+    if (count<= 0) {
+      amount = 1
+    }
+    if (count > 8) {
+      amount = 8
+    }
+    setData(text.slice(0,amount))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section-center">
+      <h3>Lorem Ipsum</h3>
+      <form action="" className="lorem-form" onSubmit={submitHandler}>
+        <label htmlFor="amount">Paragraph: </label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+       
+          {data.map((item, index) => {
+            return  <p key={index}>{item}</p>
+          })}
+        
+      </article>
+    </section>
+  )
 }
 
-export default App;
+export default App
